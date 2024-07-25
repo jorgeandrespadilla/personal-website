75c3cad334b34f6724e5fb3a78e7a38deac9918d
Jorge Andres Padilla <jorgeandres.padillasalgado@gmail.com>
2024-07-25 01:04:30 -0500

    feat: add base template files

---
+++ src/data/socials.ts
@@ -0,0 +1,21 @@
+import IconGithub from "../components/icons/github.astro";
+import IconLinkedin from "../components/icons/linkedin.astro";
+import IconX from "../components/icons/x.astro";
+
+export const SOCIALS = [
+  {
+    name: "Github",
+    url: "https://github.com/jorgeandrespadilla",
+    icon: IconGithub,
+  },
+  {
+    name: "Linkedin",
+    url: "https://www.linkedin.com/in/jorgeandrespadilla",
+    icon: IconLinkedin,
+  },
+  {
+    name: "X",
+    url: "https://x.com/jorgeapadilla01",
+    icon: IconX,
+  },
+];
---
+++ src/scripts/main.ts
@@ -0,0 +1,5 @@
+import contextCursor from "@/lib/context-cursor/index";
+
+contextCursor({
+  radius: 25, //will change the radius/size of the cursor
+});
