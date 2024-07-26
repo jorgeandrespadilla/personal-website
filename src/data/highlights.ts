b94c97c36570793638c2c569fc282b5dbf4086ef
Jorge Andres Padilla <jorgeandres.padillasalgado@gmail.com>
2024-07-26 10:49:57 -0500

    feat: add higlights section to landing page

---
+++ src/data/highlights.ts
@@ -0,0 +1,14 @@
+import type { Props as HighlightProp } from "../components/card/props";
+
+export const HIGHLIGHTS: HighlightProp[] = [
+    {
+        title: "Artificial Intelligence",
+        description: "Shaping the future with Machine Learning & Generative AI.",
+        actionButton: "Read More",
+        actionButtonLink: "/ai",
+    },
+    {
+        title: "Web Development",
+        description: "Building the web with modern technologies to enhance user experiences.",
+    }
+];
